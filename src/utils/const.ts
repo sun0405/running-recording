881db69a035643e682ccaa11f03bf8c79405a233
sun0405 <1367121328@qq.com>
2023-10-11 12:12:14 +0800

    你的提交信息

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -25,9 +25,9 @@ const MAP_LAYER_LIST = [
 ];
 
 // styling: set to `true` if you want dash-line route
-const USE_DASH_LINE = true;
+const USE_DASH_LINE = false;
 // styling: route line opacity: [0, 1]
-const LINE_OPACITY = 0.4;
+const LINE_OPACITY = 0.6;
 // styling: map height
 const MAP_HEIGHT = 600;
 //set to `false` if you want to hide the road label characters
