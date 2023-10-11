7c811717d80e9f7f7e89caa49ee3b5e9ae5283d2
sun0405 <1367121328@qq.com>
2023-10-11 10:43:26 +0800

    你的提交信息

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,29 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const data: ISiteMetadataResult = {
+  siteTitle: 'Running Page',
+  siteUrl: 'https://sun0405.github.io/',
+  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'Blog',
+      url: 'https://sun0405.github.io/',
+    },
+    {
+      name: 'About',
+      url: 'https://sun0405.github.io/',
+    },
+  ],
+};
+
+export default data;
